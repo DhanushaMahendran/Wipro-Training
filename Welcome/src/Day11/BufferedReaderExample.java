15731be4f4dfecdb9d5d4767d27abe4f153057d6
Dhanusha Mahendran <dsha99852@gmail.com>
2025-08-06 22:58:50 +0530

    SDLC

--- Welcome/src/Day11/BufferedReaderExample.java
+++ Welcome/src/Day11/BufferedReaderExample.java
@@ -7,18 +7,18 @@ import java.io.IOException;
 public class BufferedReaderExample {
     public static void main(String[] args) {
         try {
-            // Open the file using BufferedReader
-            BufferedReader reader = new BufferedReader(new FileReader("sample.txt"));
+            
+            BufferedReader reader = new BufferedReader(new FileReader("\"C:\\Users\\Dhanusha\\OneDrive\\Desktop\\Wipro-Training\\Welcome\\sample.txt\""));
 
             String line;
             System.out.println("Reading file using BufferedReader:\n");
 
-            // Read line by line
+           
             while ((line = reader.readLine()) != null) {
                 System.out.println(line);
             }
 
-            // Close the file
+            
             reader.close();
         } catch (IOException e) {
             System.out.println("Error reading file.");
